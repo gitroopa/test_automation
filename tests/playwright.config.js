bfc67b29b50c18d700dfac87de73daffd74af4e1
Roopa Adivappa <Roopa.Adivappa@bionic.co.uk>
2023-10-13 09:30:37 +0100

    forInterview

--- tests/playwright.config.js
+++ tests/playwright.config.js
@@ -14,14 +14,14 @@ const config = {
             name: 'Chromium',
             use:{ browserName: 'chromium'}       
         },
-        // {
-        //     name: 'Firefox',
-        //     use:{ browserName: 'firefox'}
-        // },
-        // {
-        //     name: 'Webkit',
-        //     use:{ browserName: 'webkit'},
-        // }
+        {
+            name: 'Firefox',
+            use:{ browserName: 'firefox'}
+        },
+        {
+            name: 'Webkit',
+            use:{ browserName: 'webkit'},
+        }
 
 
         
